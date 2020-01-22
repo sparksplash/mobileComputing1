e60ebe4fa9fd57ea7dd02f4467fdfcded052eb19
sparksplash <sparksplash77@gmail.com>
2020-01-22 21:41:27 +0900

    a

---
+++ app/src/main/java/com/example/mobilecomputing1/FAB3.kt
@@ -0,0 +1,32 @@
+package com.example.mobilecomputing1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class FAB3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fab3)
+
+        val fab1_button = findViewById(R.id.button2) as Button
+        fab1_button.setOnClickListener {
+            val intent = Intent(this, FAB1::class.java)
+            startActivity(intent)
+        }
+
+        val fab2_button = findViewById(R.id.button3) as Button
+        fab2_button.setOnClickListener {
+            val intent = Intent(this, FAB2::class.java)
+            startActivity(intent)
+        }
+
+        val fab3_button = findViewById(R.id.button4) as Button
+        fab3_button.setOnClickListener {
+            Toast.makeText(this, "Current Activity", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
