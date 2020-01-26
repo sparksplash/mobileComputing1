2d5ce85003eea5662510c3537b5eb8b792509c86
sparksplash <sparksplash77@gmail.com>
2020-01-27 08:40:44 +0900

    a

---
+++ app/src/main/java/com/example/mobilecomputing1/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobilecomputing1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
